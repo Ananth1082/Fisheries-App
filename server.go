54f981cd4c21742f66ad5b74ff0678f05b09c256
Ananth1082 <ananthshetty203@gmail.com>
2024-05-31 11:10:20 +0530

    Added some locations

--- server.go
+++ server.go
@@ -1,13 +1,37 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type glocation struct {
+	Name      string  `gorm:"column:name"`
+	Longitude float64 `gorm:"column:longitude"`
+	Latitude  float64 `gorm:"column:latitude"`
+}
 
 func main() {
+	dsn := "postgresql://ananth:b9BTvlD_4VeqvxpqzbneMw@poster-app-4864.7s5.aws-ap-south-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database", err)
+	}
+	var locations []glocation
+
+	if err := db.Find(&locations).Error; err != nil {
+		log.Fatal(err)
+	}
+	for _, loc := range locations {
+		fmt.Printf("Name: %s, \n", loc.Name)
+	}
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	r.GET("/locations", func(ctx *gin.Context) {
+		ctx.JSON(200, locations)
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run()
 }
